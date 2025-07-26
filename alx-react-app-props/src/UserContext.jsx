ed75acb15827af1cc9ba9cfd6895ed9fe540d0cd
NjungeNicholas <J77-3874-2023@student.mksu.ac.ke>
2025-07-27 01:58:28 +0300

    feat:  Reconfigured prop drilling using Context API

---
+++ alx-react-app-props/src/UserContext.jsx
@@ -0,0 +1,14 @@
+// Create a UserContext:
+// Create a new file UserContext.js.
+// Initialize a Context using React.createContext() and export it.
+// Provide Context in App:
+// In App.jsx, import UserContext and wrap the ProfilePage component inside UserContext.Provider. Pass userData as the value to the provider.
+// Consume Context in UserDetails:
+// Modify UserDetails.jsx to consume UserContext using the useContext hook instead of receiving userData as a prop.
+// Remove Unused Props:
+// Remove the userData props passed through ProfilePage and UserInfo, as these will no longer be necessary.
+import React from 'react';
+
+const DataContext = React.createContext();
+export const UserContext = DataContext;
+
