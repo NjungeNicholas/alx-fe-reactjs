def691fc780548487b6b858aa9d789bf79c547e3
NjungeNicholas <J77-3874-2023@student.mksu.ac.ke>
2025-08-17 02:44:31 +0300

    feat: implement Search component for GitHub User Search functionality

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Search from './components/search'
 
 function App() {
   return (
     <>
       <h1>GitHub User Search</h1>
+      <Search />
     </>
   )
 }
---
+++ github-user-search/src/components/search.jsx
@@ -0,0 +1,59 @@
+import {useState}  from "react";
+import { fetchUserData } from "../services/githubService";
+
+function Search() {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [userData, setUserData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    const handleInputChange = (e) => {
+      setSearchTerm(e.target.value); // Capture user input
+    };
+
+    const handleSubmit = (e) => {
+      e.preventDefault(); // Prevent form submission
+      setLoading(true);
+      setError(false);
+      setUserData(null);
+
+      fetchUserData(searchTerm)
+        .then((data) => {
+          setUserData(data);
+          setLoading(false);
+        })
+        .catch(() => {
+          setError(true);
+          setLoading(false);
+        });
+    };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter GitHub username"
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {loading && <p>Loading...</p>}
+      {error && <p>Looks like we can't find the user.</p>}
+      {userData && (
+        <div>
+            <h2>{userData.name}</h2>
+            <img src={userData.avatar_url} alt={`${userData.name}'s avatar`} />
+            <p>{userData.name}</p>
+            <a style={{maxWidth: '50%'}} href={userData.html_url} target="_blank" rel="noopener noreferrer">
+              View Profile
+            </a>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Search;
